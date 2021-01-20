8e04a5047f2c10aae2e7abfb26edab0341207f78
Walter Jacob <jacobw56@gmail.com>
2021-01-20 13:09:48 -0500

    First working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import YTNails from './YTNails.js';
+import banner from './banner.png';
+import {
+  //Breadcrumb,
+  //Button,
+  Image,
+  Layout,
+  //Menu,
+  Typography
+} from 'antd';
 import './App.css';
 
-function App() {
+const {
+  //Header,
+  Footer,
+  //Sider,
+  Content
+} = Layout;
+const { Title } = Typography;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Content style={{ padding: '0 50px' }}>
+        {/* <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb> */}
+        <Image alt="Overkill Projects Limited" src={banner} preview={false} />
+        <Title level={5} style={{padding: '10px 10px'}}>
+          Here at Overkill Projects, we help businesses and individuals take their ideas from concept to reality. Whatever your technology needs, we have the domain experience necessary to help you achieve your goals.
+        </Title>
+        <Title level={3} style={{padding: '10px 10px'}}>Services:</Title>
+        <Title level={5} style={{padding: '10px 10px'}}>
+        <ul>
+          <li>Prototyping and MVP design</li>
+          <li>Circuit design</li>
+          <li>PCB design and analysis</li>
+          <li>Device optimization</li>
+          <li>Manufacturing and assembly</li>
+          <li>Consultation services</li>
+        </ul>
+        </Title>
+        <Title level={3} style={{padding: '10px 10px'}}>Contact:</Title>
+        <Title level={5} style={{padding: '10px 10px'}}>Feel free to reach out to us anytime by <a href='mailto:overkill.projects.limited@gmail.com'>email</a> or by phone at 856 405 5932.</Title>
+        <Title level={3} style={{padding: '10px 10px'}}>Latest from the YouTube channel:</Title>
+        <YTNails />
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Overkill Projects Limited ©2021</Footer>
+    </Layout>
   );
 }
 
---
+++ src/YTNails.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Card, Image, Row, Col } from 'antd';
+
+const { Meta } = Card;
+const baseURL = 'https://youtube.googleapis.com/youtube/v3/playlistItems?'; // part=snippet&playlistId=UUfb1xoLtuLp5rOLEcJ2Y_YQ&maxResults=4&key=AIzaSyDejdMcPh15Ao7UN1YdkPVNOuvKIvGrzT4
+const query = {
+  part: 'snippet',
+  playlistId: 'UUfb1xoLtuLp5rOLEcJ2Y_YQ',
+  maxResults: '4',
+  key: 'AIzaSyDejdMcPh15Ao7UN1YdkPVNOuvKIvGrzT4',
+};
+
+function YTNails() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    /* Build url */
+    let url = baseURL;
+    for (let [key, value] of Object.entries(query)) {
+      url = url + key + '=' + value + '&';
+    }
+    url = url.slice(0, -1);
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.items);
+          console.log(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, []);
+
+  return <Row>{
+    items.map((video, idx) =>
+      <Col xs={24} sm={24} md={12} lg={12} xl={6} xxl={6} key={idx}>
+        <Card
+          hoverable
+          style={{padding: '10px'}}
+          cover={<Image alt='youtube_thumb' preview={false} src={video.snippet.thumbnails.high.url} />}
+        >
+          <Meta title={video.snippet.title} />
+        </Card>
+      </Col>)
+  }</Row>;
+}
+
+export default YTNails;
